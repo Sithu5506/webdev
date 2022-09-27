2d9b7362edbfadfc56466a38685144f5d56c954e
wangq5506 <wangq5506@gmail.com>
2022-09-27 12:48:10 +0630

    update

---
+++ java.js
@@ -0,0 +1,6 @@
+function clickFun(){
+    window.open("https://percylabtest.online/label4.html","theFrame")
+}
+function helpFun(){
+    window.open("https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics")
+}
