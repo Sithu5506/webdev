7ce9ab7629eb5ffe99e76f8f637bec6ea67b4f6b
wangq5506 <wangq5506@gmail.com>
2022-10-15 15:34:25 +0630

    update

--- java.js
+++ java.js
@@ -1,5 +1,5 @@
 function clickFun(){
-    window.open("https://percylabtest.online/label4.html","theFrame")
+    window.open("http://www.percylabtest.online:8081/label4.html","theFrame")
 }
 function helpFun(){
     window.open("https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics")
